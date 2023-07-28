0cd2736f2ce67283cb62717111df4e56618ac719
lokideos <archmage@hey.com>
2023-07-28 15:58:10 +0600

    feat | load next 5 popular tracks

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,18 @@
 class ArtistsController < ApplicationController
   def show
+    limit = params[:limit].present? ? params[:limit].to_i : 5
     artist = Artist.find(params[:id])
     albums = selected_albums(artist.albums, params[:album_type]).with_attached_cover.preload(:artist)
-    tracks = artist.tracks.popularity_ordered.limit(5)
+    tracks = artist.tracks.popularity_ordered.limit(limit)
+    next_track_exists = artist.tracks.popularity_ordered.count > limit
 
     if turbo_frame_request?
-      render partial: "discography", locals: {artist:, albums:}
+      case turbo_frame_request_id
+      when /discography/ then render partial: "discography", locals: {artist:, albums:, next_track_exists:}
+      when /tracks/ then render partial: "popular_tracks", locals: {artist:, tracks:, limit:, next_track_exists:}
+      end
     else
-      render action: :show, locals: {artist:, albums:, tracks:}
+      render action: :show, locals: {artist:, albums:, tracks:, next_track_exists:}
     end
   end
 
